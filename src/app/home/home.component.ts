6f24a70fb60f1150cfa836dc4aef4f5e0570d6d5
Gennadiy Krol <gennadiy.krol@gmail.com>
2017-07-26 07:56:33 +0300

    #1. Create an empty Home/Dashboard screen

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AppState } from '../app.service';
+import { Title } from './title';
+import { EmailCellEditor } from './email.cell-editor';
+
+@Component({
+	selector: 'home',
+	styleUrls: ['./home.component.scss'],
+	templateUrl: './home.component.html'
+})
+export class HomeComponent {
+
+}
