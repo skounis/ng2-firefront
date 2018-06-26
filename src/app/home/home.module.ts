b1c36f4ce19ab442526ea922f40d9a942a1a99e8
Stavros Kounis <skounis@gmail.com>
2018-06-26 11:39:35 +0300

    feat(*) Improve scrolling and sidebar #16

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../common/shared.module';
 import { HomeComponent } from './home.component';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
 	imports: [
 		CommonModule,
-		SharedModule
+		SharedModule,
+		RouterModule
 	],
 	declarations: [HomeComponent],
 })
