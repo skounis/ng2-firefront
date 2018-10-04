973cd12b32fac88bbc1ee0d8cbf603a81adf9e2c
orfeasr <orfeasced@gmail.com>
2018-10-04 15:00:45 +0300

    chore(app) Issue #34 test all samples and clean up the code

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -2,15 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from '../about/about.component';
-import { AboutModule } from '../about/about.module';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { SharedModule } from '../common/shared.module';
 import { HomeComponent } from '../home/home.component';
 import { HomeModule } from '../home/home.module';
 import { managerRoutes } from '../manager/manager.module';
-import { ReportsComponent } from '../reports/reports.component';
-import { ReportsModule } from '../reports/reports.module';
 import { ShellComponent } from './shell.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 
@@ -22,8 +18,6 @@ const routes: Routes = [
 		children: [
 			...managerRoutes,
 			{ path: '', component: HomeComponent, pathMatch: 'full' },
-			{ path: 'reports', component: ReportsComponent, pathMatch: 'full' },
-			{ path: 'about', component: AboutComponent, pathMatch: 'full' }
 		]
 	}
 ];
@@ -38,8 +32,6 @@ const routes: Routes = [
 		FormsModule,
 		RouterModule.forChild(routes),
 		SharedModule,
-		ReportsModule,
-		AboutModule,
 		HomeModule
 	],
 	providers: []
