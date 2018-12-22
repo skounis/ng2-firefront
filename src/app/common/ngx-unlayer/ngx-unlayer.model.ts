960cc9c49ec678b594d4c70d7f54b92ffd3f4fb9
Stavros Kounis <skounis@gmail.com>
2018-12-22 17:33:51 +0200

    chore(unlayer) Create models and const

--- src/app/common/ngx-unlayer/ngx-unlayer.model.ts
+++ src/app/common/ngx-unlayer/ngx-unlayer.model.ts
@@ -4,3 +4,18 @@ export class Options {
 	tools: Object;
 	designTags: Object;
 }
+
+export class DesignData {
+	id?: string;
+	name?: string;
+	$key?: string;
+	design: any;
+}
+
+export const TEMPLATE_TYPE_SYSTEM = 'TEMPLATE_TYPE_SYSTEM';
+export const TEMPLATE_TYPE_USER = 'TEMPLATE_TYPE_USER'
+
+export class TemplateWithType {
+	type: string;
+	template: DesignData;
+}
