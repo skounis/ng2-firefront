49d27cbb360eabc6a9e22b70ff846b8c4bf931d6
Stavros Kounis <skounis@gmail.com>
2018-12-22 17:35:25 +0200

    feat(unlayer templates list) Use interfaces

--- src/app/common/ngx-unlayer/ngx-unlayer-templates.component.ts
+++ src/app/common/ngx-unlayer/ngx-unlayer-templates.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter, OnInit, OnChanges, SimpleChange, SimpleChanges} from '@angular/core';
+import { DesignData, TemplateWithType, TEMPLATE_TYPE_SYSTEM, TEMPLATE_TYPE_USER } from './ngx-unlayer.model'
 import { NgxUnlayerRestService } from './ngx-unlayer.service';
 
 @Component({
@@ -7,9 +8,11 @@ import { NgxUnlayerRestService } from './ngx-unlayer.service';
   styleUrls: ['./ngx-unlayer-templates.component.scss']
 })
 export class NgxUnlayerTemplatesComponent implements OnInit, OnChanges {
+  TEMPLATE_TYPE_SYSTEM = TEMPLATE_TYPE_SYSTEM;
+  TEMPLATE_TYPE_USER = TEMPLATE_TYPE_USER;
 
 	@Input() userDesigns = [];
-	@Output() onTemplateSelected = new EventEmitter();
+	@Output() onTemplateSelected = new EventEmitter<TemplateWithType>();
 
   constructor(public service: NgxUnlayerRestService) { }
   systemTemplates: [];
@@ -30,9 +33,9 @@ export class NgxUnlayerTemplatesComponent implements OnInit, OnChanges {
     console.log('got name: ', _userDesigns.currentValue);
     this.userDesigns = _userDesigns.currentValue;
 	}
-	
-	selectTemplate(type: string, template: any) {
-		let templateWithType = {
+
+	selectTemplate(type: string, template: DesignData) {
+		let templateWithType: TemplateWithType = {
 			type: type,
 			template: template
 		}
